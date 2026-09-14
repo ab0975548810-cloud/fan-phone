9676810a9ce1e479b528621878e6bdb0231f79f9
ab0975548810-cloud <ab0975548810@gmail.com>
2026-09-14 20:05:18 +0800

    Install shared AI outpaint API patch

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -33,15 +33,17 @@ def post_worker_init(worker):
         from template_editor_patch import install as install_template_editor
         from order_management_patch import install as install_order_management
         from quality_perf_patch import install as install_quality_perf
+        from ai_outpaint_patch import install as install_ai_outpaint
         install_security(app_module)
         install_supabase_resilience(app_module)
         install_quality_perf(app_module)
+        install_ai_outpaint(app_module)
         install_admin_perf(app_module)
         install_order_colors(app_module)
         install_asset_categories(app_module)
         install_template_editor(app_module)
         install_order_management(app_module)
-        worker.log.info('Benfuwan security/performance/quality middleware installed')
+        worker.log.info('Benfuwan security/performance/quality/AI middleware installed')
     except Exception:
         worker.log.exception('Failed to install Benfuwan security/performance middleware')
         raise
